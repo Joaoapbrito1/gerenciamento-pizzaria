827ad1e40dc34b4dbea5ae8d466205da4efd8ce6
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-21 16:43:30 -0300

    refactor: update ClienteControllers class.

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -1,23 +1,26 @@
 package com.zup.pizzaria.controllers;
 
-import com.zup.pizzaria.models.Cliente;
-import com.zup.pizzaria.repository.ClienteRepository;
+import com.zup.pizzaria.dtos.ClienteDTO;
+import com.zup.pizzaria.dtos.ClienteRequestDTO;
+import com.zup.pizzaria.services.ClienteService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-    private final ClienteRepository clienteRepository;
 
-    public ClienteController(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clienteRepository.save(cliente);
-        return ResponseEntity.status(201).body(clienteSalvo);
+    public ResponseEntity<ClienteDTO> criarCliente(@Valid @RequestBody ClienteRequestDTO clienteRequestDTO) {
+        ClienteDTO clienteDTO = clienteService.criarCliente(clienteRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteDTO);
     }
 }
