2af60333d5b17d92b728ca56311eb904b6ac3a8b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-21 16:14:36 -0300

    feat: @valid's authentic order.

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -3,14 +3,12 @@ package com.zup.pizzaria.controllers;
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.repository.ClienteRepository;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-
     private final ClienteRepository clienteRepository;
 
     public ClienteController(ClienteRepository clienteRepository) {
@@ -18,8 +16,8 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> criarCliente(@RequestBody @Valid Cliente cliente) {
+    public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody Cliente cliente) {
         Cliente clienteSalvo = clienteRepository.save(cliente);
-        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
+        return ResponseEntity.status(201).body(clienteSalvo);
     }
 }
