649325a62222566efea586399f0d0ac6b3d3635f
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-21 16:43:57 -0300

    refactor: update Cliente class.

--- src/main/java/com/zup/pizzaria/models/Cliente.java
+++ src/main/java/com/zup/pizzaria/models/Cliente.java
@@ -4,63 +4,40 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 @Entity
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O nome não pode ser vazio ou nulo.")
     private String nome;
-
-    @NotBlank(message = "O e-mail não pode ser vazio.")
-    @Email(message = "O e-mail deve ser válido.")
     private String email;
-
-    @NotBlank(message = "O telefone não pode ser vazio.")
-    @Pattern(regexp = "\\d+", message = "O telefone deve conter apenas números.")
-    @Size(min = 8, message = "O telefone deve ter no mínimo 8 dígitos.")
     private String telefone;
+
+    public Cliente() {
+    }
 
     public Cliente(String nome, String email, String telefone) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
     }
+
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getTelefone() {
         return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 }
