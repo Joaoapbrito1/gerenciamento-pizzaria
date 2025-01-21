2cf6815fe2ad6b5062023aef61e4b57edb4ea8a7
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-21 16:17:38 -0300

    feat: edit exception handling.

--- src/main/java/com/zup/pizzaria/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zup/pizzaria/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.zup.pizzaria.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +13,15 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions
+            (@org.jetbrains.annotations.NotNull MethodArgumentNotValidException ex)
+    {
         Map<String, String> errors = new HashMap<>();
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        return errors;
     }
 }
