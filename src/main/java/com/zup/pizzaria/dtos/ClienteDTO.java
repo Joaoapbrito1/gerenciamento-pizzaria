fa3438d6f27a6cd57503dc919d7a14555c2b29b0
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-21 16:45:07 -0300

    feat: implements CustomerDTO class.

---
+++ src/main/java/com/zup/pizzaria/dtos/ClienteDTO.java
@@ -0,0 +1,32 @@
+package com.zup.pizzaria.dtos;
+
+public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public ClienteDTO(Long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
