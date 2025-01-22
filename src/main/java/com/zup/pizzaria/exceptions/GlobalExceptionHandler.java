e90c2afc550670b60d3fce83a0454ea789f77ce6
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-22 09:18:38 -0300

    refactor: edit the GlobalExceptionHandler class.

--- src/main/java/com/zup/pizzaria/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zup/pizzaria/exceptions/GlobalExceptionHandler.java
@@ -1,27 +1,30 @@
 package com.zup.pizzaria.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions
-            (@org.jetbrains.annotations.NotNull MethodArgumentNotValidException ex)
-    {
-        Map<String, String> errors = new HashMap<>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            errors.put(error.getField(), error.getDefaultMessage());
-        }
-        return errors;
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> erros = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        Map<String, Object> resposta = new HashMap<>();
+        resposta.put("mensagem", "Erro de validação");
+        resposta.put("erros", erros);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
     }
 }
